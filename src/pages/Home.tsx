7591ec1151215540024e6b632b3a2e8d57a6284e
Andresalfaro7 <andresalfaroparada@gmail.com>
2025-08-26 13:49:28 -0600

    Configuración para GitHub Pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import AnimationTextOne from "../components/AnimationTextOne";
+import SlideInText from "../components/SlideInText";
+
+const Home = () => {
+    return (
+        <div className="flex h-screen items-center p-10">
+            <div className="w-1/2">
+                <nav className="text-3xl">
+                    <li className="w-fit"><Link to={'/'}><SlideInText text="Home" delay={0}/></Link></li>
+                    <li className="w-fit"><Link to={'/proyectos'}><SlideInText text="Proyectos" delay={100}/></Link></li>
+                    <li className="w-fit"><Link to={'/stack'}><SlideInText text="Stack" delay={200}/></Link></li>
+                    <li className="w-fit"><Link to={'/contactos'}><SlideInText text="Contactos" delay={300}/></Link></li>
+                </nav>
+            </div>
+            <div className="w-1/2">
+                <h1 className="md:h-20 lg:h-28 xl:h-36"><AnimationTextOne text="Andrés A." speed={100} /></h1>
+                <h3><AnimationTextOne text="Fullstack Developer" speed={100} /></h3>
+                <p><AnimationTextOne text="Apasionado por la tecnología y el desarrollo de soluciones innovadoras." speed={50} /></p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  HOME: '/',
+  PROJECTS: '/proyectos',
+  STACK: '/stack',
+  CONTACT: '/contactos',
+  // LOGIN: '/login',
+  // DASHBOARD: {
+  //   BASE: '/dashboard',
+  //   HOME: '/dashboard/home',
+  //   SETTINGS: '/dashboard/settings',
+  // },
+};
