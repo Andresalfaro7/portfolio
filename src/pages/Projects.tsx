d533f194bc7399b9d5a1a0ef31e23c803f3d4ff7
Andresalfaro7 <andresalfaroparada@gmail.com>
2026-02-07 17:21:09 -0600

    Padding in project responsive mobile

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import dataProjects from '../data/dataProjects.json';
 
 const Projects = () => {
     return (
-        <div className="">
+        <div className="pb-10 md:pb-1">
             <h1 className="text-center p-8"><AnimationTextOne text="Proyectos" speed={100} /></h1>
             <div className="min-h-dvh px-10 xl:px-20 py-4 sm:py-12 md:py-24 lg:py-24 xl:py-32 lg:-ml-4 xl:ml-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 xl:gap-x-0 gap-y-24 md:gap-y-72 justify-between">
                 {dataProjects.projects.map((project) => (
